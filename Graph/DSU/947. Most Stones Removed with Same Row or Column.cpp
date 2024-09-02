696f6fa586c8e9a42aa853a17d59800bde8fe3be
Abhinay Kumar <kabhinayak02@gmail.com>
2024-09-02 19:48:51 +0530

    Add dsu prob

---
+++ Graph/DSU/947. Most Stones Removed with Same Row or Column.cpp
@@ -0,0 +1,206 @@
+/*
+Using DSU 
+
+tc: o(n)
+sc: o(n)
+*/
+class DisjointSet {
+    vector<int> rank, parent, size;
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+
+/*
+DSU Approach 
+
+tc: o(n^2)
+sc: o(n)
+*/
+class DisjointSet {
+public: 
+    vector<int> rank, parent, size;
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+class Solution {
+public:
+    int removeStones(vector<vector<int>>& stones) {
+        int n = stones.size();
+
+        DisjointSet ds(n);
+
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                if(stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]){
+                    ds.unionByRank(i, j);
+                }
+            }
+        }
+        
+        int groups = 0;
+        for(int i=0; i<n; i++){
+            if(ds.parent[i] == i){
+                groups++;
+            }
+        }
+        return n - groups;
+    }
+};
+
+class Solution {
+public:
+    int removeStones(vector<vector<int>>& stones) {
+        int n = stones.size();
+        int maxRow = 0;
+        int maxCol = 0;
+
+        for(auto it: stones){
+            maxRow = max(maxRow, it[0]);
+            maxCol = max(maxCol, it[1]);
+        }
+        DisjointSet ds(maxRow + maxCol + 1);
+        unordered_map<int,int> stoneNodes;
+        for(auto it: stones){
+            int nodeRow = it[0];
+            int nodeCol = it[1] + maxRow + 1;
+            ds.unionBySize(nodeRow, nodeCol);
+            stoneNodes[nodeRow] = 1;
+            stoneNodes[nodeCol] = 1;
+        }
+
+        int cnt = 0;
+        for(auto it: stoneNodes){
+            if(ds.findUPar(it.first) == it.first){
+                cnt++;
+            }
+        }
+        return n - cnt;
+    }
+};
+
+
+/*
+Using DFS 
+
+tc: o(n)
+sc: o(n)
+*/
+class Solution {
+public:
+    void dfs(int ind, vector<vector<int>>& stones, vector<bool>& visited){
+        visited[ind] = true;
+
+        for(int i=0; i<stones.size(); i++){
+            int r = stones[i][0];
+            int c = stones[i][1];
+
+            if(!visited[i] && (stones[ind][0] == r || stones[ind][1] == c)){
+                dfs(i, stones, visited);
+            }
+        }
+    }
+    int removeStones(vector<vector<int>>& stones) {
+        int n = stones.size();
+
+        vector<bool> visited(n, false);
+        int groups = 0;
+
+        for(int i=0; i<n; i++){
+            if(!visited[i]){
+                dfs(i, stones, visited);  
+                groups++;
+            }
+        }
+        return n - groups;
+    }
+};
