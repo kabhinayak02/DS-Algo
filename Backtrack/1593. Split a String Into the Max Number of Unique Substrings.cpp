dd06da18a6016d4da38b1f09664748aa114bbeb1
Abhinay Kumar <kabhinayak02@gmail.com>
2024-10-21 23:09:38 +0530

    add backtracking

--- Backtrack/1593. Split a String Into the Max Number of Unique Substrings.cpp
+++ Backtrack/1593. Split a String Into the Max Number of Unique Substrings.cpp
@@ -1,5 +1,8 @@
 /*
-using backtracking 
+using backtracking
+
+In the end of the each recursive call we will get the unique string in the set, so size of the set gives the 
+number of ans. 
 
 tc: o(2^n)
 sc: o(n)
@@ -9,6 +12,41 @@ class Solution {
 public:
     void solve(int idx, int n, int &ans, string &s, set<string>& st){
         if(idx == n){
+            ans = max(ans, (int)st.size());
+            return;
+        }
+        for(int j=idx; j<n; j++){
+            string str = s.substr(idx, j-idx+1);
+            if(!st.count(str)){
+                st.insert(str);
+                solve(j+1, n, ans, s, st);
+                st.erase(str);
+            }
+        }
+    }
+    int maxUniqueSplit(string s) {
+        int n = s.length();
+        int ans = 1;
+        set<string> st;
+
+        solve(0, n, ans, s, st);
+        return ans; 
+    }
+};
+
+
+/*
+using backtracking with optimisation 
+
+tc: o(2^n)
+sc: o(n)
+*/
+class Solution {
+public:
+    void solve(int idx, int n, int &ans, string &s, set<string>& st){
+        if((int)st.size() + (n - idx) <= ans) return;
+
+        if(idx >= n){
             ans = max(ans, (int)st.size());
             return;
         }
