edaaee2d7e5bc9616bdcc12b45dd13b7701a9433
Abhinay Kumar <kabhinayak02@gmail.com>
2024-11-05 21:24:24 +0530

    add string

---
+++ String/2914. Minimum Number of Changes to Make Binary String Beautiful.cpp
@@ -0,0 +1,17 @@
+/*
+
+tc: o(n)
+sc: o(1)
+*/
+class Solution {
+public:
+    int minChanges(string s) {
+        int n = s.length(), cnt = 0;
+        for(int i=1; i<n; i+=2){
+            if(s[i] != s[i-1]){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
