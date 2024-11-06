1cd53b172f0ffbd9b96f1c7f65116e7d4133df69
Abhinay Kumar <kabhinayak02@gmail.com>
2024-11-06 20:55:14 +0530

    update array question

--- Array /3011. Find if Array Can Be Sorted.cpp
+++ Array /3011. Find if Array Can Be Sorted.cpp
@@ -32,3 +32,46 @@ public:
         return true;
     }
 };
+
+/*
+
+make a group of same bit count array or think it like a subarray of same bit counts, now compare the max Element from 
+the previuos subarray to min Element from the current subarray, if CurrMinEle > prevMaxElement then element from 
+their subarray remains in their own subarray, no element exchange will happen. 
+
+
+tc: o(n)
+sc: o(1)
+*/
+
+class Solution {
+public:
+    bool canSortArray(vector<int>& nums) {
+        int n = nums.size();
+        int check = __builtin_popcount(nums[0]);
+        int maxEle = INT_MIN, prevMaxEle = INT_MIN;
+        int minEle = INT_MAX;
+        
+        for(int i = 0; i < n;) {
+            // Find max and min elements in current group with same bit count
+            while(i < n && check == __builtin_popcount(nums[i])) {
+                maxEle = max(maxEle, nums[i]);
+                minEle = min(minEle, nums[i]);
+                i++;
+            }
+            
+            // Check if current group's minimum is less than previous group's maximum
+            if(prevMaxEle != INT_MIN && minEle < prevMaxEle) 
+                return false;
+            
+            // Update for next group
+            prevMaxEle = maxEle;
+            if(i < n) {
+                check = __builtin_popcount(nums[i]);
+                maxEle = INT_MIN;
+                minEle = INT_MAX;
+            }
+        }
+        return true;
+    }
+};
