23b983408ca6241d6965f9e6a49c3c6b64e6cf35
Abhinay Kumar <kabhinayak02@gmail.com>
2024-12-03 15:27:08 +0530

    update string question

--- String/2109. Adding Spaces to a String.cpp
+++ String/2109. Adding Spaces to a String.cpp
@@ -1,5 +1,6 @@
 /*
-simple traversal 
+String traversal: traverse the string if the spaces element (i.e. index for string) matches with the given s string index 
+add the space and then continue to add elements
 
 tc: o(n+m)
 sc: o(n) for storing the answer
