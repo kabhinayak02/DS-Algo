122e5009aa2d68ac9f9b78549f1ae368586f50ac
Abhinay Kumar <kabhinayak02@gmail.com>
2024-12-06 23:42:40 +0530

    add sliding window

---
+++ Sliding window/1248. Count Number of Nice Subarrays.cpp
@@ -0,0 +1,34 @@
+/*
+using variable size sliding window 
+
+tc: o(n)
+sc: o(n)
+*/
+
+class Solution {
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        int n = nums.size();
+        int i = 0, j = 0;
+        int cntOdd = 0, ans = 0;
+        int countLeft = 0; // Tracks subarrays starting from valid positions
+        
+        while (j < n) {
+            if (nums[j] % 2 != 0) {
+                cntOdd++;
+                countLeft = 0; // Reset count of valid subarrays
+            }
+            
+            while (cntOdd == k) {
+                if (nums[i] % 2 != 0) cntOdd--;
+                countLeft++;
+                i++;
+            }
+            
+            ans += countLeft; // Add valid subarrays ending at j
+            j++;
+        }
+        
+        return ans;
+    }
+};
